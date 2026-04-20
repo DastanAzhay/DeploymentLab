8c00a248f74f8cd1e54ee7535b7b6def593148e0
DastanAzhay <dastanazhay09@gmail.com>
2026-04-20 14:35:35 +0500

    trying to change image alignment

--- app1.py
+++ app1.py
@@ -4,6 +4,8 @@ st.title("Hello, Streamlit!")
 st.write("This is a simple Streamlit app.")
 name = st.text_input("Enter your name:")
 img = Image.open("flag.png")
-st.image(img, caption="Sample Image")
+
 if name:
-    st.write(f"Hello, {name}!") 
+    st.write(f"Hello, {name}!")
+with st.container(horizontal_alignment="center"):
+    st.image(img, caption="Flag Image", use_column_width=True)
