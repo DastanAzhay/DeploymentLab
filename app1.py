87c07cd51465460dcb7d30ced581be52469bc7d5
DastanAzhay <159790586+DastanAzhay@users.noreply.github.com>
2026-04-20 09:48:19 +0000

    changed 5.0

--- app1.py
+++ app1.py
@@ -3,9 +3,10 @@ from PIL import Image
 st.title("Hello, Streamlit!")
 st.write("This is a simple Streamlit app.")
 name = st.text_input("Enter your name:")
+if name:
+    st.write(f"Hello, {name}!")
+
 img = Image.open("flag.png")
 with st.container(horizontal_alignment="center"):
     st.image(img, caption="Flag Image", use_column_width=True) 
 
-if name:
-    st.write(f"Hello, {name}!")
