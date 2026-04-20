1a50b27a9d61fdeef3b19f84992493a9288dce96
DastanAzhay <dastanazhay09@gmail.com>
2026-04-20 14:27:55 +0500

    image added 2.0

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ from PIL import Image
 st.title("Hello, Streamlit!")
 st.write("This is a simple Streamlit app.")
 name = st.text_input("Enter your name:")
-img = Image.open("C:/Users/dasta/Desktop/backend/flag.png")
+img = Image.open("flag.png")
 st.image(img, caption="Sample Image")
 if name:
     st.write(f"Hello, {name}!") 
