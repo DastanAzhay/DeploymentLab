cc19767f70275f05f87aa59d7db1da20707ff4b9
DastanAzhay <dastanazhay09@gmail.com>
2026-04-20 14:06:49 +0500

    changed

--- app1.py
+++ app1.py
@@ -1,6 +1,3 @@
 import streamlit as st
 st.title("Hello, Streamlit!")
 st.write("This is a simple Streamlit app.")
-name = st.text_input("Enter your name:")
-if name:
-    st.write(f"Hello, {name}!")
